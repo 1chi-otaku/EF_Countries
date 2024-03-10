e9156631ef2d04d0570e0d15a2d53b10ef5a8755
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-03-10 17:56:57 +0200

    v2

--- EF_Countries/Program.cs
+++ EF_Countries/Program.cs
@@ -1,4 +1,5 @@
-﻿using EF_Countries.Context;
+﻿using Azure;
+using EF_Countries.Context;
 using EF_Countries.Entities;
 using Microsoft.IdentityModel.Tokens;
 
@@ -13,16 +14,19 @@ namespace CodeFirst.LazyLoading
                 while (true)
                 {
                     Console.Clear();
-                    Console.WriteLine("1. Show All Countries");
-                    Console.WriteLine("2. Show All Coutries Name");
-                    Console.WriteLine("3. Show All Countries Capital");
-                    Console.WriteLine("4. Show All European Countries");
-                    Console.WriteLine("5. Show All Countries with specific area");
-                    Console.WriteLine("6. Show All Coutries with a or e in name");
-                    Console.WriteLine("7. Show All Countries starting with 'A'");
-                    Console.WriteLine("8. Show All Countries by Area Range");
-                    Console.WriteLine("9. Show All Countries By Population");
-                    Console.WriteLine("0. Exit");
+                    Console.WriteLine("1.  Show All Countries");
+                    Console.WriteLine("2.  Show All Coutries Name");
+                    Console.WriteLine("3.  Show All Countries Capital");
+                    Console.WriteLine("4.  Show All European Countries");
+                    Console.WriteLine("5.  Show All Countries with specific area");
+                    Console.WriteLine("6.  Show All Coutries with a or e in name");
+                    Console.WriteLine("7.  Show All Countries starting with 'A'");
+                    Console.WriteLine("8.  Show All Countries by Area Range");
+                    Console.WriteLine("9.  Show All Countries By Population");
+                    Console.WriteLine("10. Add new Country");
+                    Console.WriteLine("11. Edit Country");
+                    Console.WriteLine("12. Remove Country");
+                    Console.WriteLine("0.  Exit");
                     int result = int.Parse(Console.ReadLine()!);
                     switch (result)
                     {
@@ -53,6 +57,15 @@ namespace CodeFirst.LazyLoading
                         case 9:
                             ShowCountriesByPopulation();
                             break;
+                        case 10:
+                            AddNewCountry();
+                            break;
+                        case 11:
+                            EditCountry();
+                            break;
+                        case 12:
+                            RemoveCountry();
+                            break;
                         case 0:
                             return;
                     };
@@ -61,6 +74,143 @@ namespace CodeFirst.LazyLoading
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void RemoveCountry()
+        {
+            Console.Clear();
+            using (var db = new Context())
+            {
+                Console.WriteLine("Enter Country Number to Edit: ");
+                var countries = db.Countries.ToList();
+
+                for (int i = 0; i < countries.Count; i++)
+                {
+                    Console.WriteLine($"Country #{i + 1} {countries[i].Name}; Capital: {countries[i].Capital}");
+                }
+
+                int number = int.Parse(Console.ReadLine()!) - 1;
+
+                var country = (from cout in db.Countries
+                               select cout).ToList()[number];
+                db.Countries.RemoveRange(country);
+                db.SaveChanges();
+            }
+        }
+
+        private static void EditCountry()
+        {
+            Console.Clear();
+
+            using (var db = new Context())
+            {
+                Console.WriteLine("Enter Country Number to Edit: ");
+                var countries = db.Countries.ToList();
+
+                for (int i = 0; i < countries.Count; i++)
+                {
+                    Console.WriteLine($"Country #{i + 1} {countries[i].Name}; Capital: {countries[i].Capital}");
+                }
+
+                int number = int.Parse(Console.ReadLine()!) - 1; 
+
+                if (number >= 0 && number < countries.Count)
+                {
+                    var countryToEdit = countries[number];
+
+                    do
+                    {
+                        Console.WriteLine("Enter Country Name: ");
+                        countryToEdit.Name = Console.ReadLine()!;
+                    }
+                    while (string.IsNullOrWhiteSpace(countryToEdit.Name));
+
+                    do
+                    {
+                        Console.WriteLine("Enter Country Capital: ");
+                        countryToEdit.Capital = Console.ReadLine()!;
+                    }
+                    while (string.IsNullOrWhiteSpace(countryToEdit.Capital));
+
+                    Console.WriteLine("Enter Country Population: ");
+                    countryToEdit.Popilation = int.Parse(Console.ReadLine()!);
+
+                    Console.WriteLine("Enter Country Area: ");
+                    countryToEdit.Area = double.Parse(Console.ReadLine()!);
+
+                    Console.WriteLine("Select Continent:");
+
+                    var continents = db.Continents.ToList();
+                    Console.Clear();
+                    for (int i = 0; i < continents.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}. {continents[i].Name}");
+                    }
+
+                    Console.WriteLine("Enter Number of Continent: ");
+                    int continentIndex = int.Parse(Console.ReadLine()!) - 1; 
+
+                    if (continentIndex >= 0 && continentIndex < continents.Count)
+                    {
+                        countryToEdit.Continent = continents[continentIndex];
+                        db.SaveChanges();
+                    }
+                    else Console.WriteLine("Invalid Continent Index.");
+
+                }
+                else Console.WriteLine("Invalid Country Index.");
+            }
+        }
+
+        private static void AddNewCountry()
+        {
+            Console.Clear();
+
+            string name, capital;
+
+            using (var db = new Context())
+            {
+                do
+                {
+                    Console.WriteLine("Enter Country Name: ");
+                    name = Console.ReadLine()!;
+                }
+                while (name.Trim().IsNullOrEmpty());
+                do
+                {
+                    Console.WriteLine("Enter Country Capital: ");
+                    capital = Console.ReadLine()!;
+                }
+                while (capital.Trim().IsNullOrEmpty());
+
+                Console.WriteLine("Enter Country Population: ");
+                int population = int.Parse(Console.ReadLine()!);
+
+                Console.WriteLine("Enter Country Area: ");
+                double area = double.Parse(Console.ReadLine()!);
+
+                Console.WriteLine("Enter Number of Continent: ");
+                var continents = db.Continents.ToList();
+
+                for (int i = 0; i < continents.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {continents[i].Name}");
+                }
+
+                int number = int.Parse(Console.ReadLine()!);
+                var continent = db.Continents.ToList()[number-1];
+
+                Country country = new Country
+                {
+                    Name = name,
+                    Capital = capital,
+                    Popilation = population,
+                    Area = area,
+                    Continent = continent
+                };
+                db.Countries?.Add(country);
+                db.SaveChanges();
             }
         }
 
